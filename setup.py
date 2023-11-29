0b31ba9c3d7bd19a426bf727205ac048a7a74635
scottwilson <scott.bradley.wilson@gmail.com>
2023-11-29 15:02:56 +0000

    build: added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='mario',
+    version='0.1',
+    packages=['mario'],
+    url='https://github.com/JiscDACT/mario',
+    license='all rights reserved',
+    author='scottwilson',
+    author_email='',
+    description='',
+    long_description='',
+    install_requires=['pandas', 'tableauhyperapi', 'pantab']
+)
