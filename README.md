3c4ecf45d259af3b7bc3f54ddfcbda56bc818af9
scottwilson <scott.bradley.wilson@gmail.com>
2023-11-30 13:04:53 +0000

    build: Added workflows and setup for PyPi

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # General purpose data pipelines 
 
-A set of base classes and helpers for common data pipeline tasks, whether using control scripts or Airflow tasks.
+A set of base classes and helpers for 
+common data pipeline tasks, whether using
+in local control scripts or in Airflow tasks.
+
+## !WARNING!
+This is a public repository for ease of deployment.
+Be especially careful not to commit anything
+that shouldn't be here.
--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup
 
 setup(
-    name='mario',
+    name='mario-pipeline-tools',
     version='0.1',
     packages=['mario'],
     url='https://github.com/JiscDACT/mario',
     license='all rights reserved',
-    author='scottwilson',
-    author_email='',
-    description='',
-    long_description='',
+    author='scottbw',
+    author_email='scott.wilson@jisc.ac.uk',
+    description='Base classes and helpers for common data pipeline tasks',
+    long_description='A set of base classes and helpers for common data pipeline tasks, '
+                     'whether using in local control scripts or in Airflow tasks.',
+    package_data={
+        # If any package contains *.ini files, include them
+        '': ['*.ini']
+    },
+    include_package_data=True,
     install_requires=['pandas', 'tableauhyperapi', 'pantab']
 )
