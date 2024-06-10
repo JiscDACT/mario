e13fa89e0857023d4b194365a80f5cfb31ec7bde
scottwilson <scott.bradley.wilson@gmail.com>
2024-06-10 23:31:25 +0100

    build: update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mario-pipeline-tools',
-    version='0.26',
+    version='0.27',
     packages=['mario'],
     url='https://github.com/JiscDACT/mario',
     license='all rights reserved',
