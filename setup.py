a33ea805de29e1f947653bc47ced83f7a028ecc7
scottwilson <scott.bradley.wilson@gmail.com>
2024-06-12 10:30:03 +0100

    build: increment revision

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mario-pipeline-tools',
-    version='0.27',
+    version='0.28',
     packages=['mario'],
     url='https://github.com/JiscDACT/mario',
     license='all rights reserved',
