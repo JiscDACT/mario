9549970c8d1ce41a4a6594fde9b78669845747ec
tiffany.cheng <tiffany.cheng@jisc.ac.uk>
2025-06-10 12:09:21 +0100

    build: update to 0.53

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mario-pipeline-tools',
-    version='0.52',
+    version='0.53',
     packages=['mario'],
     url='https://github.com/JiscDACT/mario',
     license='all rights reserved',
