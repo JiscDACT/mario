710cab5aec0d9ce16204bf6718b8eabac7dacd34
tiffany.cheng <tiffany.cheng@jisc.ac.uk>
2025-08-15 13:47:18 +0100

    build: update to 0.56 ready for next release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mario-pipeline-tools',
-    version='0.55',
+    version='0.56',
     packages=['mario'],
     url='https://github.com/JiscDACT/mario',
     license='all rights reserved',
